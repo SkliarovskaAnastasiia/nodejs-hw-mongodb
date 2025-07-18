8f43da5fa39df432040cd13db7c5852823cc1acb
Anastasiia Skliarovska <skliarovskaa@gmail.com>
2025-07-18 12:24:31 +0300

    update authenticate

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -3,20 +3,10 @@ import { sessionsCollection } from '../db/models/session.js';
 import { usersCollection } from '../db/models/user.js';
 
 export const authenticate = async (req, res, next) => {
-  const authHeader = req.get('Authorization');
+  const token = req.cookies.accessToken;
 
-  if (!authHeader)
-    return next(
-      new createHttpError.Unauthorized('Authorization header missing'),
-    );
-
-  const bearer = authHeader.split(' ')[0];
-  const token = authHeader.split(' ')[1];
-
-  if (bearer !== 'Bearer' || !token)
-    return next(
-      new createHttpError.Unauthorized('Auth header should be of type Bearer'),
-    );
+  if (!token)
+    return next(new createHttpError.Unauthorized('Access token missing'));
 
   const session = await sessionsCollection.findOne({ accessToken: token });
   if (!session)
